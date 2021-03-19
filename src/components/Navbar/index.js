a1393578a855e80e0a2be9a554e11387da23ce38
Michael Cervantes <mac95860@hotmail.com>
2021-03-18 19:39:05 -0500

    style error corrected

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 
 function Navbar() {
+
+    const [show, handleShow] = useState();
     return (
-        <div className = "nav">
+        <div className = "nav nav_black">
             <div className = "nav_contents">
                 <img className = "nav_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
                 <img className = "nav_avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt=""/>
