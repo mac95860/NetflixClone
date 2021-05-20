a93e5f0f9055bb8ea6c8ca908e3a559aea0320f0
Michael Cervantes <mac95860@hotmail.com>
2021-05-20 10:01:03 -0500

    implement firebase

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -22,7 +22,7 @@ function Navbar() {
     return (
         <div className = {`nav ${show && 'nav_black'}`}>
             <div className = "nav_contents">
-                <img className = "nav_logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
+                <img className = "nav_logo" src="../../images/netflix_logo.png" alt=""/>
                 <img className = "nav_avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt=""/>
             </div>
         </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAPSg8-0qV1lnlGTIit8PKCOGH-tPATsaI",
-    authDomain: "netflix-build-f8043.firebaseapp.com",
-    projectId: "netflix-build-f8043",
-    storageBucket: "netflix-build-f8043.appspot.com",
-    messagingSenderId: "63921085309",
-    appId: "1:63921085309:web:8e2926c8a8dc0240918a14"
-  };
+  apiKey: "AIzaSyAPSg8-0qV1lnlGTIit8PKCOGH-tPATsaI",
+  authDomain: "netflix-build-f8043.firebaseapp.com",
+  projectId: "netflix-build-f8043",
+  storageBucket: "netflix-build-f8043.appspot.com",
+  messagingSenderId: "63921085309",
+  appId: "1:63921085309:web:8e2926c8a8dc0240918a14"
+};
