05600e524ced77e8760f2b04507ee3dfa30a4767
Michael Cervantes <mac95860@hotmail.com>
2021-05-27 15:30:52 -0500

    login screen done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Login.css"
 
 function Login() {
+    const [signIn, setSignIn] = useState(false);
+
     return (
         <div className = "loginScreen">
            <div className = "loginScreen_background">
                <img className = "loginScreen_logo" src = "https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt = ""/>
-                <button className = "loginScreen_button">
+                <button onClick = {() => setSignIn(true)} className = "loginScreen_button">
                     Sign in
                 </button>
                 <div className = "loginScreen_gradient" />
@@ -15,6 +17,14 @@ function Login() {
            <div className = "loginScreen_body">
                <>
                 <h1>Unlimited films, TV programmes and more.</h1>
+                <h2>Watch anywhere. Cancel at any time.</h2>
+                <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                <div className = "loginScreen_input">
+                    <form>
+                        <input type = "email" placeholder = "Email Address"/>    
+                        <button onClick = {() => setSignIn(true)} className = "loginScreen_getStarted">GET STARTED</button>
+                    </form>    
+                </div> 
                </>
            </div>
         </div>
