046f0aedece178576e92cb2591f22a2f913eaa87
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-06-01 15:42:20 -0500

    sign in page finished

--- src/pages/SignUpScreen.js
+++ src/pages/SignUpScreen.js
@@ -3,12 +3,15 @@ import './SignUpScreen.css'
 
 function SignInScreen() {
     return (
-        <div className = "signupScreen">
+        <div className = "signUpScreen">
             <form>
                 <h1>
                     Sign In
                 </h1>
                 <input placeholder = "email" type = "email"/>
+                <input placeholder = "Password"  type = "password"/>
+                <button type = "submit">Sign In</button>
+                <h4><span className = "signUpScreen_gray">New to Netflix? </span> Sign Up Now.</h4>
             </form>
         </div>
     )
