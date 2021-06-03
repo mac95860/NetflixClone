c7dbe93e7e188f1a997841cb365b6091613a5a3c
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-06-03 16:59:30 -0500

    sign in function

--- src/pages/SignUpScreen.js
+++ src/pages/SignUpScreen.js
@@ -22,6 +22,12 @@ function SignInScreen() {
 
     const signIn = (e) => {
         e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser)
+        }).catch((error) => alert(error.message))
     }
 
     return (
