a810832e1026489b48e0c9d10a0d87e0bd228bdb
LAPTOP-R8TAHJ1O\mac95 <mac95860@hotmail.com>
2021-06-04 12:09:08 -0500

    implement redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeScreen from './pages/HomeScreen';
 import Login from './pages/Login';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom" 
+import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, login, selectUser } from "./features/userSlice"
 
 function App() {
 
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // listens to any auth state change
+      const unsubscribe = auth.onAuthStateChanged(userAuth => {
+        if(userAuth) {
+          //logged in
+          // console.log(userAuth)
+          dispatch(login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          }))
+        } else {
+          //logged out
+          dispatch(logout);
+        }
+      })
+
+      return unsubscribe;
+  }, []);
+  
   return (
     <div className="App">
      
